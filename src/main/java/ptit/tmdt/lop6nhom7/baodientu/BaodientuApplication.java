3d96d85361075621835622e9060ad5e3763d2fe3
unknown <giap200496@gmail.com>
2026-04-13 11:52:56 +0700

    F

---
+++ src/main/java/ptit/tmdt/lop6nhom7/baodientu/BaodientuApplication.java
@@ -0,0 +1,13 @@
+package ptit.tmdt.lop6nhom7.baodientu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaodientuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaodientuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ptit/tmdt/lop6nhom7/baodientu/BaodientuApplicationTests.java
@@ -0,0 +1,13 @@
+package ptit.tmdt.lop6nhom7.baodientu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaodientuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
